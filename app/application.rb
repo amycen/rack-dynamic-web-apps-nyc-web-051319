8188bf1580906e239ea02dbd19b0057039579288
Amy Cen <amy.cen@blackrock.com>
2019-06-02 06:16:13 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,17 @@ class Application
   def call(env)
     resp = Rack::Response.new
     resp.write "Hello, World"
+
+    num1 = Kernel.rand(1..20)
+    num2 = Kernel.rand(1..20)
+    num3 = Kernel.rand(1..20)
+
+    if num1 == num2 && num2 == num_3
+      puts "You Win"
+    else
+      puts "You Lose"
+    end
+    
     resp.finish
   end
 
